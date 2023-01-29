1d09eafc996b7b721e15edfc902b025ceefa4bad
Jason Chase <jchase@solidmark.com>
2023-01-29 08:18:48 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+</script>
+
+<main class="p-10 h-screen">
+  <div class="grid grid-cols-2 gap-10 h-full">
+    <p class="font-bold">Paste from Song Select:</p>
+    <p class="font-bold">Corrected output for Planning Center:</p>
+    <div
+      contenteditable="true"
+      class="border-2 rounded-lg text-2xl font-bold"
+    />
+    <textarea class="border-2 border-green-600 rounded-lg" />
+  </div>
+</main>
+
+<style>
+  .grid {
+    grid-template-rows: 0.5rem 1fr;
+  }
+</style>
